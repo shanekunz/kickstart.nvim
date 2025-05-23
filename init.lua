d88e3f87acc7f56ace4638e795a1183a78647071
Shane Kunz <shanemkunz@gmail.com>
2025-05-23 10:39:01 -0500

    this is fine for now

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -650,7 +650,7 @@ require('lazy').setup({
               },
             },
           },
-          filetypes = { 'vue' },
+          filetypes = { 'vue', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
         },
         volar = {
           init_options = {
